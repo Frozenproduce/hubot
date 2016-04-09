d4d26928dae35dcca50d3f802ef900ad36bb60b4
Hammer <john@musicglue.com>
2016-04-09 18:11:50 +0100

    return eb statuses

--- src/platform.js
+++ src/platform.js
@@ -1,11 +1,41 @@
-import aws from 'aws-sdk';
+import AWS from 'aws-sdk';
+
+const apps = [
+  'basket-service',
+  'discounts-service',
+  'events-service',
+  'graph',
+  'payment-gateways',
+  'products-service',
+  'profiles-directory',
+  'profiles-renderer',
+  'reservations',
+  'router',
+  'shrinkray',
+  'streams-service',
+  'themes-service',
+  'viewers-service',
+];
+
+function getStatusForEnv(conn, name) {
+  const params = { AttributeNames: ['All'], EnvironmentName: `${name}-blue` };
+  return conn.describeEnvironmentHealth(params).promise()
+}
+
+function writeResponse(item) {
+  return `App: ${item.EnvironmentName} - ${item.HealthStatus} - P95:${item.ApplicationMetrics.Latency.P95}s`;
+}
+
+function transformResults(payloads) {
+  return payloads.map(writeResponse);
+}
 
 export default robot => {
-  robot.respond(/deploy version ([a-z0-9]+)/, res => {
-    if (robot.auth.hasRole(res.envelope.user, 'deployer')) {
-      res.send(`ok - deploying match ${res.match[1]}`);
-    } else {
-      res.send('denied');
-    }
+  robot.respond(/platform status/, res => {
+    const conn = new AWS.ElasticBeanstalk();
+    Promise.all(apps.map(app => getStatusForEnv(conn, app)))
+      .then(transformResults)
+      .then(statuses => res.send(statuses.join('\n')))
+      .catch(err => res.send(JSON.stringify(err, null, 4)));
   });
 };
